d03fa13bdcc6566ce1bc87dac97802779ce1699e
deepanshugarg2812 <deepanshugarg2812@gmail.com>
2021-01-20 18:56:38 +0530

    Add products added

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const PORT = process.env.PORT || 9999;
 app.use(express.json()); //Data to be in json format
 app.use(express.urlencoded({extended:true}));
 
+//Setting the static file
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+
 app.get('/',(req,res) => {
   res.send("Working");
 })
@@ -15,6 +19,9 @@ app.use('/login',require('./Router/login_customer.js'));
 
 //Login onwer
 app.use('/login_owner',require('./Router/login_owner.js'));
+
+// Product route
+app.use('/products',require('./Router/products.js'));
 
 app.listen(PORT,() => {
   console.log("Server started");
